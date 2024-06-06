5e9a3f228b73eed0657ed263979339e63ce822d4
Lawrence Francis <lawrencedikeu@gmail.com>
2024-06-06 04:13:37 +0200

    Update app initial interface

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
 window.addEventListener("DOMContentLoaded", main);
 
-const topics_open_svg = `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-<path d="M13 17l5-5-5-5M6 17l5-5-5-5"/>
-</svg>
-`;
-const topics_close_svg = `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-<path d="M11 17l-5-5 5-5M18 17l-5-5 5-5"/>
-</svg>
-`;
 
 let topics_expand = false;
-let topics_svg = topics_open_svg;
 
 function main(){
-    console.log("DOM IS READY!")
-    const tool_bar_topics = document.querySelector("#tool-bar-topics")
+    console.log("DOM IS READY!");
+
+    const tool_bar_topics = document.querySelector("#tool-bar-topics");
+    const topics_svg_open = tool_bar_topics.querySelector("#topics-svg-open");
+    const topics_svg_close = tool_bar_topics.querySelector("#topics-svg-close");
+    const side_box = document.querySelector(".side-box");
+    const side_box_resizer = document.querySelector(".side-box-resizer");
+    let isResizing = false;
+    let sideBoxOriginalWidth, sideBoxBoundingBox;
+
+    topics_svg_open.style.display = "none";
+
     tool_bar_topics.addEventListener(
         "click", (event)=>{
             topics_expand = !topics_expand;
-            topics_svg = topics_expand ? topics_close_svg : topics_open_svg;
-            
-            event.target.innerHTML = topics_svg;
-            // event.target.style.backgroundColor = 
-            console.log(topics_svg);
+            if(topics_expand){
+                topics_svg_close.style.display = "none";
+                topics_svg_open.style.display = "block";
+                side_box.style.display = "none";
+            }
+            else{
+                topics_svg_close.style.display = "block";
+                topics_svg_open.style.display = "none";
+                side_box.style.display = "block";
+            }
         }
     )
-    tool_bar_topics.innerHTML = topics_open_svg;
 
+    side_box_resizer.addEventListener("mousedown", (event)=>{
+        isResizing = true;
+        sideBoxOriginalWidth = side_box.offsetWidth;
+        sideBoxBoundingBox = side_box.getBoundingClientRect();
+
+        document.addEventListener("mousemove", resizeSideBox);
+        document.addEventListener("mouseup", stopResizeSideBox);
+    })
+
+    function resizeSideBox(event){
+        let sideBoxResizerBoundingBox = side_box_resizer.getBoundingClientRect();
+        const dx = parseInt(event.clientX-(sideBoxBoundingBox.x+sideBoxBoundingBox.width-sideBoxResizerBoundingBox.width/2)); 
+        console.log(dx);
+
+        const width = Math.max(220, sideBoxOriginalWidth + dx)
+
+        side_box.style.width = width +"px";
+    }
+
+    function stopResizeSideBox(){
+        if(isResizing){
+            isResizing = false;
+            document.removeEventListener("mousemove", resizeSideBox);
+            document.removeEventListener("mouseup", stopResizeSideBox)
+        }
+        
+    }
 }
